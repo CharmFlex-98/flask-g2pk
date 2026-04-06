70c3cac15fa4792e66a18bbaf0dd4e7ce541d7b0
CharmFlex-98 <77537132+CharmFlex-98@users.noreply.github.com>
2026-04-06 12:04:13 +0800

    get mecab import, modified impl

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from konlpy.tag import Mecab
+from mecab import Mecab
 from g2pk import G2p
 
 app = Flask(__name__)
@@ -16,16 +16,27 @@ def hello_world():
 
 @app.route('/mecab')
 def get_mecab():
-    k_str = request.args.get('q', u'영등포구청역에 있는 맛집 좀 알려주세요.')
-    morphs = mecab.morphs(k_str)
-    nouns = mecab.nouns(k_str)
-    pos = mecab.pos(k_str)
-    return {'morphs': morphs, 'nouns': nouns, 'pos': pos}
+    k_str = request.args.get('q', '')
+    if not k_str:
+        return {'morphemes': []}
+    result = mecab.parse(k_str)
+
+    morphemes = [
+        {
+            'surface': m.surface, 
+            'pos': m.feature.pos,
+            'start_offset': m.span.start,
+            'end_offset': m.span.end
+        }     
+        for m in result
+    ]
+
+    return {'morphemes': morphemes}
 
 
 @app.route('/g2pk')
 def run_g2pk():
-    k_str = request.args.get('q', u'신을 신고 얼른 동사무소에 가서 혼인 신고 해라')
+    k_str = request.args.get('q', '')
     print(k_str)
     res = g2p(k_str)
     return {'result': res}
